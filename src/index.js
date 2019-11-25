b5d056a458d78dce1fed55743c1979e5b39efcf3
Marcin Delektowski <marcin.delektowski@gmail.com>
2019-11-26 00:14:29 +0100

    Clean directory

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
 import firebase from 'firebase/app';
 import 'firebase/database';
 
 // Initialize Firebase
-
 const firebaseConfig = {
     apiKey: "AIzaSyAP8J-cqMBK8ZCxsw8Xqe4rIyKbTSF41xg",
     authDomain: "farm-economy.firebaseapp.com",
@@ -16,9 +14,9 @@ const firebaseConfig = {
     messagingSenderId: "79374372851",
     appId: "1:79374372851:web:fb67af39a9170779d0cc86"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
 
 ReactDOM.render(
   <App />,
